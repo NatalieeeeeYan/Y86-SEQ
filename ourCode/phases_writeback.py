ea705ce2e7083053fac2df2f45744f89cfc6e554
Natalieeeee <105032850+NatalieeeeeYan@users.noreply.github.com>
2022-12-01 08:35:40 +0800

    Update phases_writeback.py

--- ourCode/phases_writeback.py
+++ ourCode/phases_writeback.py
@@ -2,6 +2,11 @@ from const import *
 from general_funcs import *
 
 
+
+#################################################################################
+# 六大阶段第五阶段: 写回
+# 根据 dstE 和 dstM 写回寄存器
+#################################################################################
 def writeback(cpu):
     if cpu.dstE != RNONE:
         cpu.Reg.write(cpu.dstE, cpu.valE)
